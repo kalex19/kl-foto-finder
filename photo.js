456cb1cd587c94dcc344d0a54c863d09e0d0ecdc
Katherine Lewis <kale8432@colorado.edu>
2019-02-21 10:58:22 -0700

    Initial Commit Try Two

---
+++ photo.js
@@ -0,0 +1,7 @@
+// A photo.js file that contains a Photo class.
+// Photo methods must include, but are not limited to:
+// constructor
+// saveToStorage
+// deleteFromStorage
+// updatePhoto
+// A main.js file that contains all dom related javascript.
